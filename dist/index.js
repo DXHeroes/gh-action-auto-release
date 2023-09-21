24e2daa3122565d9acfe5fb12b4654e8b00025a1
Prokop Simek <prokop@dxheroes.io>
2023-09-21 10:51:19 +0200

    chore: info

--- dist/index.js
+++ dist/index.js
@@ -9665,12 +9665,12 @@ const github = __importStar(__nccwpck_require__(5438));
 try {
     // `who-to-greet` input defined in action metadata file
     const nameToGreet = core.getInput('who-to-greet');
-    core.debug(`Hello ${nameToGreet}!`);
+    core.info(`Hello ${nameToGreet}!`);
     const time = new Date().toTimeString();
     core.setOutput('time', time);
     // Get the JSON webhook payload for the event that triggered the workflow
     const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.debug(`The event payload: ${payload}`);
+    core.info(`The event payload: ${payload}`);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
 }
 catch (error) {
