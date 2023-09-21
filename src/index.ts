41f651aacc330eaa0c0947537ed11452dd59b6ad
Prokop Simek <prokop@dxheroes.io>
2023-09-21 13:23:56 +0200

    feat: update with npm secret

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,29 @@
 import * as core from '@actions/core';
-import * as github from '@actions/github';
+
+const VALID_RELEASE_TYPES = ['test', 'node'];
 
 try {
-    // `who-to-greet` input defined in action metadata file
-    const nameToGreet = core.getInput('who-to-greet');
-    core.info(`Hello ${nameToGreet}!`);
-    const time = new Date().toTimeString();
-    core.setOutput('time', time);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    const payload = JSON.stringify(github.context.payload, undefined, 2);
-    core.info(`The event payload: ${payload}`);
+    core.info(`Validating input parameters...`);
+
+    // get input values
+    const releaseType = core.getInput('release-type');
+    const packageName = core.getInput('package-name');
+    
+    // validate release type is provided and is valid
+    if (!releaseType) {
+        throw new Error(`Release type is required.`);
+    } else {
+        if (VALID_RELEASE_TYPES.includes(releaseType)) {
+            throw new Error(`Release type '${releaseType}' is not valid. Valid values are: ${VALID_RELEASE_TYPES.join(', ')}.`);
+        }
+    }
+
+    // validate package name is provided
+    if (!packageName) {
+        throw new Error(`Package name is required.`);
+    }
+
+    core.notice(`Configuration valid. Processing release for package '${packageName}' with release type '${releaseType}'.`);
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
 } catch (error: any) {
     core.setFailed(error.message);
